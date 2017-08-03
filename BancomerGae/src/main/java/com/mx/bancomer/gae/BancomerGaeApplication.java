57f3e89f73f0c1c312c4f0ccf3229c08328b272f
MARCOK0771N <MARCO_CHARMED89@HOTMAIL.COM>
2017-08-03 01:49:19 -0500

    se versiona demo
    se versiona demo

---
+++ BancomerGae/src/main/java/com/mx/bancomer/gae/BancomerGaeApplication.java
@@ -0,0 +1,12 @@
+package com.mx.bancomer.gae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancomerGaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancomerGaeApplication.class, args);
+	}
+}
